54987575522f2df74eb632142e2ae5af90856dc2
yyokoo <revaldan1357@gmail.com>
2023-04-18 11:35:44 +0900

    model コントローラ定義

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  Rails.application.routes.draw do
+    # ヘルスチェック
+    get '/health_check', to: 'health_checks#index'
+    # 記事一覧
+    get '/blogs', to: 'blogs#index'
+    # 記事詳細  
+    get '/blogs/:id', to: 'blogs#show'
+    # 記事作成
+    post '/blogs', to: 'blogs#create'
+  end
+  
 end
